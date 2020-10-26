222eba531311186be8dcfb7406eb94816e82da26
wxsm <wxsms@foxmail.com>
2020-10-26 10:36:21 +0800

    fix: search box cover by other elements issue

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -41,8 +41,8 @@ export default {
 body
   overflow-y scroll
 
-// .content__default
-//   word-break break-all
+.search-box .suggestions
+  z-index: 10
 
 .theme-default-content:not(.custom) > *:first-child
   margin-top 1.5rem !important
